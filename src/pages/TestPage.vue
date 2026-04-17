b7fc319b1d61b2c8654d91b2a8c7e4ae1ce3d997
Londrovski <jrmat49@gmail.com>
2026-04-17 14:05:46 +0100

    Create simple test page to debug blank screen issue

---
+++ src/pages/TestPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Earthkeeper 2.0 - Test Page</h1>
+    <p>If you can see this, Quasar is working!</p>
+    <q-btn color="primary" label="Test Button" />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TestPage'
+})
+</script>
